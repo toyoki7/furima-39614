d2bec7ae40e1458209498cc158d00ce91b56210c
toyoki7 <139853640+toyoki7@users.noreply.github.com>
2023-10-03 16:35:46 +0900

    ユーザー管理機能3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 RSpec.describe User, type: :model do
     before do
       @user = FactoryBot.build(:user)
@@ -9,9 +10,19 @@ RSpec.describe User, type: :model do
         it 'すべての情報があれば登録できる' do
           expect(@user).to be_valid
         end
+        it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+          @user.password = 'test1234'
+          @user.password_confirmation = 'test1234'
+          expect(@user).to be_valid
+        end
         it 'family1とname1が全角であれば登録できる' do
           @user.family1 = '桐生'
           @user.name1 = 'ココ'
+          expect(@user).to be_valid
+        end
+        it 'family2とname2がカタカナであれば登録できる' do
+          @user.family2 = 'キリュウ'
+          @user.name2 = 'ココ'
           expect(@user).to be_valid
         end
       end
@@ -22,10 +33,10 @@ RSpec.describe User, type: :model do
           @user.valid?
           expect(@user.errors.full_messages).to include "Nickname can't be blank"
         end
-          it 'emailが空では登録できない' do
+        it 'emailが空では登録できない' do
           @user.email = ''
           @user.valid?
-          expect(@user.errors.full_messages).to include "Email can't be blank"
+          expect(@user.errors.full_messages).to include("Email can't be blank")
         end
         it '重複したemailが存在する場合は登録できない' do
           @user.save
