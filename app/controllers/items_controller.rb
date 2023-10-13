01bad8d946075c4736a54e686e8456cd29c332d3
toyoki7 <139853640+toyoki7@users.noreply.github.com>
2023-10-13 21:31:27 +0900

    商品削除機能1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_item, only: [:show, :edit, :update]
-  before_action :check_seller, only: [:edit, :update]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+  before_action :check_seller, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(:created_at)
@@ -34,6 +34,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_params
