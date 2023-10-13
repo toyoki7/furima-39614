9e1b697b6a65706013d3daadffbd2022f09265a9
toyoki7 <139853640+toyoki7@users.noreply.github.com>
2023-10-13 20:08:19 +0900

    商品情報編集機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_seller, only: [:edit, :update]
+
   def index
     @items = Item.order(:created_at)
   end
@@ -26,16 +28,24 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
+
   private
 
   def item_params
     params.require(:item).permit(:item, :image, :explanation, :category_id, :condition_id, :burden_id, :prefecture_id, :until_id, :price).merge(user_id: current_user.id)
+  end
+
+  def check_seller
+    @item = Item.find(params[:id])
+    unless @item.user. == current_user
+      redirect_to root_path, alert: "出品者以外のユーザーはアクセスできません。"
+    end
   end
 end
