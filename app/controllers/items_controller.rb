ff405457828b34507b2318d7901ea3d9cdc1833c
toyoki7 <139853640+toyoki7@users.noreply.github.com>
2023-10-19 12:27:05 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :update]
 
   def index
-    @items = Item.order(:created_at)
+    @items = Item.order(created_at: :desc)
   end
 
   def new
